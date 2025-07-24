feeab259f53f1717038e477a1191ec1b917f03be
FabianSiswanto <siswantofabian@gmail.com>
2025-07-24 21:38:27 +0700

    feat: update sample audio path

--- other/generate_voice.ts
+++ other/generate_voice.ts
@@ -1,19 +1,19 @@
 // example.mts
 import { ElevenLabsClient } from "@elevenlabs/elevenlabs-js";
+import { AddVoiceIvcResponseModel } from "@elevenlabs/elevenlabs-js/api";
 import "dotenv/config";
 import fs from "node:fs";
 
 const elevenlabs = new ElevenLabsClient();
 
-const voice = await elevenlabs.voices.ivc.create({
+const voice: AddVoiceIvcResponseModel = await elevenlabs.voices.ivc.create({
     name: "My Voice Clone",
-    // Replace with the paths to your audio files.
-    // The more files you add, the better the clone will be.
+
     files: [
         fs.createReadStream(
-            "/path/to/your/audio/file.mp3",
+            "/other/sample_audio/test.mp3",
         ),
     ],
 });
 
-console.log(voice.voice_id);
+console.log(voice.voiceId);
